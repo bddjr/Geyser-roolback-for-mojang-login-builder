0dffa49e7ff9f097aa085b7901720ed7f71848a2
bddjr <ygyh5019@outlook.com>
2023-10-26 12:54:06 +0800

    fix

--- builder.bat
+++ builder.bat
@@ -2,6 +2,7 @@ title Geyser-roolback-for-mojang-login-builder
 cd /d %~dp0
 
 rmdir /S /Q build\
+if exist build\ goto PAUSE
 
 git clone -b build https://github.com/bddjr/Geyser-roolback-for-mojang-login-builder build
 if %errorlevel% neq 0 goto PAUSE
